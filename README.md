972f340def8cb5da6ee331d293ea322e8ceba499
Seth Ladd <sethladd@gmail.com>
2014-08-29 10:51:37 -0700

    first commit of SDK and Dartium

---
+++ README.md
@@ -0,0 +1,42 @@
+# Dart for Homebrew
+
+This is the official [Dart][] tap for [homebrew][].
+
+Mac users can use these formulae to easily install and update Dart SDK and
+Dartium. Both dev and stable channels are supported.
+
+## Initial setup
+
+If you don't have homebrew, install it from their [homepage][homebrew].
+
+Then, add this tap:
+
+```
+brew tap dart-lang/dart
+```
+
+## Installing
+
+To install the Dart SDK:
+
+```
+brew install dartsdk
+```
+
+For web developers, we highly recommend Dartium:
+
+```
+brew install dartium
+```
+
+## Updating
+
+Simply run:
+
+```
+brew upgrade dartsdk
+brew upgrade dartium
+```
+
+[homebrew]: http://brew.sh/
+[dart]: https://www.dartlang.org
---
+++ dartium.rb
@@ -0,0 +1,39 @@
+require 'formula'
+
+class Dartium < Formula
+  homepage "https://www.dartlang.org"
+
+  version '1.6.0'
+
+  url 'https://storage.googleapis.com/dart-archive/channels/stable/release/39553/dartium/dartium-macos-ia32-release.zip'
+  sha256 "25ecdbf0b80f362611fa76051db2cbff453bf8bd6fecaf2f71f6614d6907198d"
+
+  devel do
+    url 'https://storage.googleapis.com/dart-archive/channels/dev/release/39537/dartium/dartium-macos-ia32-release.zip'
+    sha256 "9e80fb04093f12e066b91a8c5ded550ad4c365ff718d21d3f5492c5c1ba2c61f"
+
+    version '1.6.0-dev.9.7'
+  end
+  
+  def shim_script target
+    <<-EOS.undent
+      #!/bin/bash
+      open "#{prefix}/#{target}" "$@"
+    EOS
+  end
+
+  def install
+    prefix.install Dir['*']
+    (bin+"dartium").write shim_script "Chromium.app"
+  end
+
+  def caveats; <<-EOS.undent
+     To use with IntelliJ, set the Dartium execute home to:
+        #{prefix}/Chromium
+    EOS
+  end
+
+  test do
+    system "#{bin}/dartium"
+  end
+end
