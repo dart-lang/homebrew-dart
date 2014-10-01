3d5913903cbdff7e4b6de5c58e50480bc1b688f5
William Hesse <whesse@google.com>
2014-10-01 15:54:20 +0200

    Refactor dart homebrew recipe so version and hashes can be updated easily.
    BUG=
    R=sgjesse@google.com
    
    Review URL: https://codereview.chromium.org//609863004

--- dart.rb
+++ dart.rb
@@ -3,25 +3,26 @@ require 'formula'
 class Dart < Formula
   homepage 'https://www.dartlang.org/'
 
+  require_relative 'data/dev_info'
+  require_relative 'data/stable_info'
+
+  version DartStable::VERSION
   if MacOS.prefer_64_bit?
-    version '1.6.0'
-    url 'https://storage.googleapis.com/dart-archive/channels/stable/release/39553/sdk/dartsdk-macos-x64-release.zip'
-    sha256 '98fba491b86e70d7fc44ed69977b365b96f9d7d79a3a95a89553df9aafaf7f81'
+    url "https://storage.googleapis.com/dart-archive/#{DartStable::SDK64_FILE}"
+    sha256 DartStable::SDK64_HASH
   else
-    version '1.6.0'
-    url 'https://storage.googleapis.com/dart-archive/channels/stable/release/39553/sdk/dartsdk-macos-ia32-release.zip'
-    sha256 '2ad33e57098fb567c6b627d149899ad301de88f03edc92c74611956642eca382'
+    url "https://storage.googleapis.com/dart-archive/#{DartStable::SDK32_FILE}"
+    sha256 DartStable::SDK32_HASH
   end
 
   devel do
+    version DartDev::VERSION
     if MacOS.prefer_64_bit?
-      version '1.7.0-dev.1.0'
-      url 'https://storage.googleapis.com/dart-archive/channels/dev/release/39799/sdk/dartsdk-macos-x64-release.zip'
-      sha256 'c721e896b313ae391a0601cc8a6a85fde0be96c107eae9677b752da9503b3f04'
+      url "https://storage.googleapis.com/dart-archive/#{DartDev::SDK64_FILE}"
+      sha256 DartDev::SDK64_HASH
     else
-      version '1.7.0-dev.1.0'
-      url 'https://storage.googleapis.com/dart-archive/channels/dev/release/39799/sdk/dartsdk-macos-ia32-release.zip'
-      sha256 '2019455ed5b5e48d20e34c3ea9f233aa2a23a442a4025211e729d41e631f8166'
+      url "https://storage.googleapis.com/dart-archive/#{DartDev::SDK32_FILE}"
+      sha256 DartDev::SDK32_HASH
     end
   end
 
--- dartium.rb
+++ dartium.rb
@@ -3,14 +3,17 @@ require 'formula'
 class Dartium < Formula
   homepage "https://www.dartlang.org"
 
-  version '1.6.0'
-  url 'https://storage.googleapis.com/dart-archive/channels/stable/release/39553/dartium/dartium-macos-ia32-release.zip'
-  sha256 '1dfe8fefde53620d4917e82cf32a585e3f2faa663f70ba3f9c84fce9da925834'
+  require_relative 'data/dev_info'
+  require_relative 'data/stable_info'
+
+  version DartStable::VERSION
+  url "https://storage.googleapis.com/dart-archive/#{DartStable::DARTIUM_FILE}"
+  sha256 DartStable::DARTIUM_HASH
 
   devel do
-    version '1.7.0-dev.1.0'
-    url 'https://storage.googleapis.com/dart-archive/channels/dev/release/39799/dartium/dartium-macos-ia32-release.zip'
-    sha256 '674d212c15edf0a2d33c8ab05f9bd11ae8def747c64f015752f8f3a0dd9ed5dd'
+    version DartDev::VERSION
+    url "https://storage.googleapis.com/dart-archive/#{DartDev::DARTIUM_FILE}"
+    sha256 DartDev::DARTIUM_HASH
   end
 
   def shim_script target
