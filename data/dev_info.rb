fbc30dc65a6146e2f74cd625ccdad2444cb48fba
Rico Wind <ricow@google.com>
2014-10-02 11:13:11 +0200

    Updated dev branch to revision 40806

--- data/dev_info.rb
+++ data/dev_info.rb
@@ -1,9 +1,9 @@
 module DartDev
-  SDK64_FILE = "channels/dev/release/40675/sdk/dartsdk-macos-x64-release.zip"
-  SDK64_HASH = "7371eb59e9450411026615ce18be1f38ff83b89c12b0fe6dc81ed187f28a1fcd"
-  SDK32_FILE = "channels/dev/release/40675/sdk/dartsdk-macos-ia32-release.zip"
-  SDK32_HASH = "a9cedc5f1dbe3d6826133d6a61bd97249d45fcf8a119c3a8c6836a0dcb459654"
-  DARTIUM_FILE = "channels/dev/release/40675/dartium/dartium-macos-ia32-release.zip"
-  DARTIUM_HASH = "37fe75e3d5097e0e3ada32a79ba0d251dfd6cf15ddda0dcc75b3de2c85a4bba4"
-  VERSION = "1.7.0-dev.4.0"
+  SDK64_FILE = "channels/dev/release/40806/sdk/dartsdk-macos-x64-release.zip"
+  SDK64_HASH = "231b4bb520f122da905bb4aaf106ee97cf11545c5ee059b93d7922b134241e0d"
+  SDK32_FILE = "channels/dev/release/40806/sdk/dartsdk-macos-ia32-release.zip"
+  SDK32_HASH = "4c7bc6c60d9d2b08ec2d4e6d7a8370530f0c7e5ddefdfe21762e161344da7436"
+  DARTIUM_FILE = "channels/dev/release/40806/dartium/dartium-macos-ia32-release.zip"
+  DARTIUM_HASH = "ce5a5c2b7cfb4fa94a5811afbb324342afd454d02c7fcd29ed219f929a29d448"
+  VERSION = "1.7.0-dev.4.1"
 end
