726e9cf9dfa4f09b6e9b49433259a7130214d68d
William Hesse <whesse@google.com>
2015-10-21 13:30:03 +0200

    Updated dev branch to revision 1.13.0-dev.7.2

--- dart.rb
+++ dart.rb
@@ -16,25 +16,25 @@ class Dart < Formula
   option 'with-dartium', 'Download and install Dartium -- Chromium with Dart'
 
   devel do
-    version '1.13.0-dev.7.1'
+    version '1.13.0-dev.7.2'
     if MacOS.prefer_64_bit?
-      url 'https://storage.googleapis.com/dart-archive/channels/dev/release/1.13.0-dev.7.1/sdk/dartsdk-macos-x64-release.zip'
-      sha256 '504f8c9a95b3f2dcf0799cf6aaab4b57ae7b3c1460048893f5a6357821b57a29'
+      url 'https://storage.googleapis.com/dart-archive/channels/dev/release/1.13.0-dev.7.2/sdk/dartsdk-macos-x64-release.zip'
+      sha256 'e1e0db76834bad0963a0f291a3501e8ad5656cb90540cba465605cd38178ab22'
     else
-      url 'https://storage.googleapis.com/dart-archive/channels/dev/release/1.13.0-dev.7.1/sdk/dartsdk-macos-ia32-release.zip'
-      sha256 '1034f32440b9973f03ceea5d441162cc7d0ae57dc5b16694917eaf2760fb087a'
+      url 'https://storage.googleapis.com/dart-archive/channels/dev/release/1.13.0-dev.7.2/sdk/dartsdk-macos-ia32-release.zip'
+      sha256 '17e633bfeb33a5a09905b16ec13a21e81da89006f688bdc3a3077deffc02337e'
     end
 
     resource 'content_shell' do
-      version '1.13.0-dev.7.1'
-      url 'https://storage.googleapis.com/dart-archive/channels/dev/release/1.13.0-dev.7.1/dartium/content_shell-macos-ia32-release.zip'
-      sha256 'e66527ea3ddb08904a6f567a8902fab8f4b21364ae38a267a15f0a57da53ce15'
+      version '1.13.0-dev.7.2'
+      url 'https://storage.googleapis.com/dart-archive/channels/dev/release/1.13.0-dev.7.2/dartium/content_shell-macos-ia32-release.zip'
+      sha256 'dc765c32e2ad11b58c1ee4cac77f30a8a4086add3a31a6ac5af5de8c074194ff'
     end
 
     resource 'dartium' do
-      version '1.13.0-dev.7.1'
-      url 'https://storage.googleapis.com/dart-archive/channels/dev/release/1.13.0-dev.7.1/dartium/dartium-macos-ia32-release.zip'
-      sha256 '8fd059fe121e94d1185c61cb20edf2c4f61f94fa16e8e57f2519b5b6bb94184b'
+      version '1.13.0-dev.7.2'
+      url 'https://storage.googleapis.com/dart-archive/channels/dev/release/1.13.0-dev.7.2/dartium/dartium-macos-ia32-release.zip'
+      sha256 '34fb8c831cd0b494df73b700f8ea395fc4aea1627bb5ae2b8f24cd99693110bb'
     end
   end
 
