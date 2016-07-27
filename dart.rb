ee9d47bb0c460150cc5a167dbbab4125cb90374f
William Hesse <whesse@google.com>
2016-07-28 01:25:52 +0200

    Updated stable branch to revision 1.18.0

--- dart.rb
+++ dart.rb
@@ -3,13 +3,13 @@ require 'formula'
 class Dart < Formula
   homepage 'https://www.dartlang.org/'
 
-  version '1.17.1'
+  version '1.18.0'
   if MacOS.prefer_64_bit?
-    url 'https://storage.googleapis.com/dart-archive/channels/stable/release/1.17.1/sdk/dartsdk-macos-x64-release.zip'
-    sha256 'a12fba33300e4b3b93e2a462818a0a5c9fbc94a48e5a655dfbc6a3078d9c5f17'
+    url 'https://storage.googleapis.com/dart-archive/channels/stable/release/1.18.0/sdk/dartsdk-macos-x64-release.zip'
+    sha256 '8ca53818d7b4a15d7247623243245d8a092642d3014b1c66e34db175e9ccc0a4'
   else
-    url 'https://storage.googleapis.com/dart-archive/channels/stable/release/1.17.1/sdk/dartsdk-macos-ia32-release.zip'
-    sha256 'aff0c8d3b4b0dde087ceb12c2bb4ff636612effbf072a6fb0e6321f682b98d76'
+    url 'https://storage.googleapis.com/dart-archive/channels/stable/release/1.18.0/sdk/dartsdk-macos-ia32-release.zip'
+    sha256 '0db92f4465494d1e6d9b23d11bd241691bcd396ce2965a4c97490f6d5b99897d'
   end
 
   option 'with-content-shell', 'Download and install content_shell -- headless Dartium for testing'
@@ -39,15 +39,15 @@ class Dart < Formula
   end
 
   resource 'content_shell' do
-    version '1.17.1'
-    url 'https://storage.googleapis.com/dart-archive/channels/stable/release/1.17.1/dartium/content_shell-macos-ia32-release.zip'
-    sha256 'adae72c29e34f2bcca1abd6700761f71d7bcd94ed4b44093ae05beeb4b1e8c83'
+    version '1.18.0'
+    url 'https://storage.googleapis.com/dart-archive/channels/stable/release/1.18.0/dartium/content_shell-macos-ia32-release.zip'
+    sha256 '320c711e09631a75b3d5c68eb83d5abf5435ce60dc1894b805d9c6ba75e42cb3'
   end
 
   resource 'dartium' do
-    version '1.17.1'
-    url 'https://storage.googleapis.com/dart-archive/channels/stable/release/1.17.1/dartium/dartium-macos-ia32-release.zip'
-    sha256 'b9210407d2432f6e11c6f39eb654ff90a9c85b47afd11600a07af6be8efe8d6e'
+    version '1.18.0'
+    url 'https://storage.googleapis.com/dart-archive/channels/stable/release/1.18.0/dartium/dartium-macos-ia32-release.zip'
+    sha256 '66d0d3e51579cd422396b1c14a4afaa9f9db0549453f482311d42c51400b9373'
   end
 
   def install
