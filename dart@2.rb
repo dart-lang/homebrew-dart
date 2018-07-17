0fd73ce43d5eee70e3addf864c523111a048cb0e
William Hesse <whesse@google.com>
2018-07-17 16:49:41 +0200

    Updated dev branch to revision 2.0.0-dev.69.0

--- dart@2.rb
+++ dart@2.rb
@@ -1,16 +1,16 @@
 class DartAT2 < Formula
   desc "The Dart 2 SDK"
   homepage "https://www.dartlang.org/"
-  version "2.0.0-dev.68.0"
+  version "2.0.0-dev.69.0"
 
   keg_only :versioned_formula
 
   if MacOS.prefer_64_bit?
-    url "https://storage.googleapis.com/dart-archive/channels/dev/release/2.0.0-dev.68.0/sdk/dartsdk-macos-x64-release.zip"
-    sha256 "d0c4748ed453d82588986f36bb6ba06a67b0c2cafc8eafaab49b7aacb8a3c664"
+    url "https://storage.googleapis.com/dart-archive/channels/dev/release/2.0.0-dev.69.0/sdk/dartsdk-macos-x64-release.zip"
+    sha256 "b376957f9cd4069443c5880066ff5b5d117f3450393de7ad57f06148e7a6b92c"
   else
-    url "https://storage.googleapis.com/dart-archive/channels/dev/release/2.0.0-dev.68.0/sdk/dartsdk-macos-ia32-release.zip"
-    sha256 "7bced650f92581f607709dce140df87111d54df9e176c802dbf2e8f37afd94a8"
+    url "https://storage.googleapis.com/dart-archive/channels/dev/release/2.0.0-dev.69.0/sdk/dartsdk-macos-ia32-release.zip"
+    sha256 "e0f87d85fc73245cc50d1ee79ff7ab3881055b6bd550bf453f7ed3aa66933326"
   end
 
   def install
