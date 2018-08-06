4a1f43c1c7ad2082d64287883eaafa642300905b
William Hesse <whesse@google.com>
2018-08-06 12:10:37 +0200

    Updated stable branch to revision 2.0.0

--- dart.rb
+++ dart.rb
@@ -2,13 +2,13 @@ class Dart < Formula
   desc "The Dart SDK"
   homepage "https://www.dartlang.org/"
 
-  version "1.24.3"
+  version "2.0.0"
   if MacOS.prefer_64_bit?
-    url "https://storage.googleapis.com/dart-archive/channels/stable/release/1.24.3/sdk/dartsdk-macos-x64-release.zip"
-    sha256 "3419869401184d1ebf44e8947de36ac83ff614097c2c52a80792e89a25c18cd8"
+    url "https://storage.googleapis.com/dart-archive/channels/stable/release/2.0.0/sdk/dartsdk-macos-x64-release.zip"
+    sha256 "7cb9e65cea94ce23b05af4e5224ec416b26c3fb6bf0718778b68f6a73e617cc3"
   else
-    url "https://storage.googleapis.com/dart-archive/channels/stable/release/1.24.3/sdk/dartsdk-macos-ia32-release.zip"
-    sha256 "387fb5e1a1231b219a599d2d7efe250387e041d6b4822ec1ddbf364794762097"
+    url "https://storage.googleapis.com/dart-archive/channels/stable/release/2.0.0/sdk/dartsdk-macos-ia32-release.zip"
+    sha256 "da55f8fce70ca46e97304810406c89f039464be909b9b92f13986ce918da6775"
   end
 
   devel do
@@ -22,43 +22,10 @@ class Dart < Formula
     end
   end
 
-  option "with-content-shell", "Download and install content_shell -- headless Dartium for testing"
-  option "with-dartium", "Download and install Dartium -- Chromium with Dart"
-
-  resource "content_shell" do
-    version "1.24.3"
-    url "https://storage.googleapis.com/dart-archive/channels/stable/release/1.24.3/dartium/content_shell-macos-x64-release.zip"
-    sha256 "01efc473c68aed830307d1dafb0cbcbfe77f40ceeeab3ef3ebe58a9912d05b13"
-  end
-
-  resource "dartium" do
-    version "1.24.3"
-    url "https://storage.googleapis.com/dart-archive/channels/stable/release/1.24.3/dartium/dartium-macos-x64-release.zip"
-    sha256 "188a038bd6367fddb434338bf6549bae25f5ad89b2f5b462acf8fb1fa20a3916"
-  end
-
   def install
     libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/dart"
     bin.write_exec_script Dir["#{libexec}/bin/{pub,dart?*}"]
-
-    if build.with? "dartium"
-      if build.devel?
-        odie "dartium is no longer supported with --devel builds. Remove --with-dartium and try again."
-      end
-      dartium_binary = "Chromium.app/Contents/MacOS/Chromium"
-      prefix.install resource("dartium")
-      (bin+"dartium").write shim_script dartium_binary
-    end
-
-    if build.with? "content-shell"
-      if build.devel?
-        odie "content-shell is no longer supported with --devel builds. Remove --with-content-shell and try again."
-      end
-      content_shell_binary = "Content Shell.app/Contents/MacOS/Content Shell"
-      prefix.install resource("content_shell")
-      (bin+"content_shell").write shim_script content_shell_binary
-    end
   end
 
   def shim_script(target)
