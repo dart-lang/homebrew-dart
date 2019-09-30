59f37904ccc00fedd1a1b1e5a820def44735b710
Alexander Thomas <athom@google.com>
2019-09-30 17:09:43 +0200

    Updated dev branch to revision 2.6.0-dev.4.0

--- dart.rb
+++ dart.rb
@@ -12,13 +12,13 @@ class Dart < Formula
   end
 
   devel do
-    version "2.6.0-dev.3.0"
+    version "2.6.0-dev.4.0"
     if Hardware::CPU.is_64_bit?
-      url "https://storage.googleapis.com/dart-archive/channels/dev/release/2.6.0-dev.3.0/sdk/dartsdk-macos-x64-release.zip"
-      sha256 "b674b79d36b18a763e1cdc72af29e99e7720fd68a25a6fb741c5feccbc512af2"
+      url "https://storage.googleapis.com/dart-archive/channels/dev/release/2.6.0-dev.4.0/sdk/dartsdk-macos-x64-release.zip"
+      sha256 "60d53dec31fd3eb7597a645e950303358c4499a0cc437112a80ae9fa6e946f62"
     else
-      url "https://storage.googleapis.com/dart-archive/channels/dev/release/2.6.0-dev.3.0/sdk/dartsdk-macos-ia32-release.zip"
-      sha256 "c8e4c32f17e136096b2e9305e13b32e6d80a7b9b8fe34026c9dd4f08fabd40a0"
+      url "https://storage.googleapis.com/dart-archive/channels/dev/release/2.6.0-dev.4.0/sdk/dartsdk-macos-ia32-release.zip"
+      sha256 "6cb4056940838a7f667bea097f66e0f952ad5af202853a0e452bc9b80f7bcf60"
     end
   end
 
