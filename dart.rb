82810decc0f6cd308e050b6a961ffcd2d3070875
Kevin Moore <kevmoo@users.noreply.github.com>
2020-04-05 17:04:56 -0700

    Remove `shim_script` function (#74)
    Only used to link to content-shell and dartium, which are not in v2+

--- dart.rb
+++ dart.rb
@@ -56,13 +56,6 @@ class Dart < Formula
     bin.write_exec_script Dir["#{libexec}/bin/{pub,dart?*}"]
   end
 
-  def shim_script(target)
-    <<~EOS
-      #!/usr/bin/env bash
-      exec "#{prefix}/#{target}" "$@"
-    EOS
-  end
-
   def caveats
     <<~EOS
       Please note the path to the Dart SDK:
