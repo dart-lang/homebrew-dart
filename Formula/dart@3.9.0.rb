35939087413af6e35678a19ea75ddb9d99ee2efb
Dart CI <dart-internal-deps-roller@dart-ci-internal.iam.gserviceaccount.com>
2025-08-13 06:03:30 -0700

    Updated stable channel to version 3.9.0

--- Formula/dart@3.9.0.rb
+++ Formula/dart@3.9.0.rb
@@ -1,33 +1,11 @@
 # typed: false
 # frozen_string_literal: true
 
-class Dart < Formula
+class DartAT390 < Formula
   desc "SDK"
   homepage "https://dart.dev"
 
-  head do
-    version "3.10.0-81.0.dev" # dev
-    if OS.mac? && Hardware::CPU.intel?
-      url "https://storage.googleapis.com/dart-archive/channels/dev/release/3.10.0-81.0.dev/sdk/dartsdk-macos-x64-release.zip"
-      sha256 "b10f420b56ac2247c4a2451eaf7eb41fdd90650df5e744ac6a216a738f1d4035"
-    elsif OS.mac? && Hardware::CPU.arm?
-      url "https://storage.googleapis.com/dart-archive/channels/dev/release/3.10.0-81.0.dev/sdk/dartsdk-macos-arm64-release.zip"
-      sha256 "e55d472047aeddf564f340d2c0e2e2fc371e55c6ca6402c47b5d9c9f261906bb"
-    elsif OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://storage.googleapis.com/dart-archive/channels/dev/release/3.10.0-81.0.dev/sdk/dartsdk-linux-x64-release.zip"
-      sha256 "1bcfbaf68b867605b9b053c47eed8c83b3db700dc5589658e93dcb2474a1b00a"
-    elsif OS.linux? && Hardware::CPU.arm?
-      if Hardware::CPU.is_64_bit?
-        url "https://storage.googleapis.com/dart-archive/channels/dev/release/3.10.0-81.0.dev/sdk/dartsdk-linux-arm64-release.zip"
-        sha256 "eda713c70e24797366379a9f67d8e18dd0a63537143a4082709d75938a9c6a0b"
-      else
-        url "https://storage.googleapis.com/dart-archive/channels/dev/release/3.10.0-81.0.dev/sdk/dartsdk-linux-arm-release.zip"
-        sha256 "793fceb6520c598bfb85829de5c5671908d7c6802c57eb4d71bde90543a42783"
-      end
-    end
-  end
-
-  conflicts_with "dart-beta", because: "dart-beta ships the same binaries"
+  keg_only :versioned_formula
   if OS.mac? && Hardware::CPU.intel?
     url "https://storage.googleapis.com/dart-archive/channels/stable/release/3.9.0/sdk/dartsdk-macos-x64-release.zip"
     sha256 "ad1ce35fff59b4eda1ff9d7e89d1e5d31cdab86192f3bdaf93baa36f5ae4d8bf"
