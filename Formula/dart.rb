bf660b76aa777d596a19d064fbbe8d78a8bd1cb8
なつき <i@ntk.me>
2025-08-21 13:43:42 -0700

    Remove exec script for non-executable files

--- Formula/dart.rb
+++ Formula/dart.rb
@@ -50,7 +50,7 @@ class Dart < Formula
   def install
     libexec.install Dir["*"]
     bin.install_symlink "#{libexec}/bin/dart"
-    bin.write_exec_script Dir["#{libexec}/bin/{pub,dart?*}"]
+    bin.write_exec_script Dir["#{libexec}/bin/{pub,dart?*}"].select { |f| File.executable?(f) }
   end
 
   def caveats
