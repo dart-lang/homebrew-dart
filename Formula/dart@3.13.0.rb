ce537ef89287be89278feb9e87b7cf8d60596dd3
Dart CI <dart-internal-deps-roller@dart-ci-internal.iam.gserviceaccount.com>
2026-08-12 01:04:24 -0700

    Updated stable channel to version 3.13.0

--- Formula/dart@3.13.0.rb
+++ Formula/dart@3.13.0.rb
@@ -1,33 +1,11 @@
 # typed: false
 # frozen_string_literal: true
 
-class Dart < Formula
+class DartAT3130 < Formula
   desc "SDK"
   homepage "https://dart.dev"
 
-  head do
-    version "3.14.0-110.0.dev" # dev
-    if OS.mac? && Hardware::CPU.intel?
-      url "https://storage.googleapis.com/dart-archive/channels/dev/release/3.14.0-110.0.dev/sdk/dartsdk-macos-x64-release.zip"
-      sha256 "161a2dc162d2d5b120bf0e2b1799f64689b19fbccac26ebc8a41f2608094ba6f"
-    elsif OS.mac? && Hardware::CPU.arm?
-      url "https://storage.googleapis.com/dart-archive/channels/dev/release/3.14.0-110.0.dev/sdk/dartsdk-macos-arm64-release.zip"
-      sha256 "52063d4d9f4f4369ab07c31608da0a44d2f52deb74779bf1e3702036fb69eaa5"
-    elsif OS.linux? && Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://storage.googleapis.com/dart-archive/channels/dev/release/3.14.0-110.0.dev/sdk/dartsdk-linux-x64-release.zip"
-      sha256 "231bee831c176081ceb388d74287a8a0b0a6e9edc2028a8c6654471d2e61894b"
-    elsif OS.linux? && Hardware::CPU.arm?
-      if Hardware::CPU.is_64_bit?
-        url "https://storage.googleapis.com/dart-archive/channels/dev/release/3.14.0-110.0.dev/sdk/dartsdk-linux-arm64-release.zip"
-        sha256 "54c26c245d465e2e277a858436b145ddbcf8b7bdfca0bf9ad85dc57a59916be0"
-      else
-        url "https://storage.googleapis.com/dart-archive/channels/dev/release/3.14.0-110.0.dev/sdk/dartsdk-linux-arm-release.zip"
-        sha256 "fbb87474a18699fdcc8d0a15b3d9d3c04932dc911179b729524274c51f36719e"
-      end
-    end
-  end
-
-  conflicts_with "dart-beta", because: "dart-beta ships the same binaries"
+  keg_only :versioned_formula
   if OS.mac? && Hardware::CPU.intel?
     url "https://storage.googleapis.com/dart-archive/channels/stable/release/3.13.0/sdk/dartsdk-macos-x64-release.zip"
     sha256 "6f75f0d6bc10c00457fb5b8861b26eadc5d883cfd11aa26a8c4f6353af370f47"
